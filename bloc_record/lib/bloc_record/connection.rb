8747b5d3b9d179c23aade0e02e80a7f0319fc2c9
Drew-Mace <dremac1@live.com>
2017-10-25 16:00:29 -0400

    checkpoint 5

--- bloc_record/lib/bloc_record/connection.rb
+++ bloc_record/lib/bloc_record/connection.rb
@@ -1,8 +1,56 @@
 require 'sqlite3'
 
-module Connection
- def connection
-   puts BlocRecord.instance_methods
-   @connection ||= SQLite3::Database.new(BlocRecord.database_filename)
+ module BlocRecord
+   class Collection < Array
+
+     def update_all(updates)
+       ids = self.map(&:id)
+
+       self.any? ? self.first.class.update(ids, updates) : false
+     end
+
+     def take(attrs, value)
+       attrs = BlocRecord::Utility.convert_keys(attrs)
+       attrs.delete "id"
+       value = values.map { |key| BlocRecord::Utility.sql_strings(value[key]) }
+
+        output = connection.execute <<-SQL
+          SELECT #{attrs}
+          FROM #{table}
+          WHERE #{value};
+        SQL
+
+      output
+     end
+
+     def where(attrs, value)
+       attrs = BlocRecord::Utility.convert_keys(attrs)
+       attrs.delete "id"
+       value = value.map { |key| BlocRecord::Utility.sql_strings(value[key]) }
+
+       output = connection.execute <<-SQL
+        SELECT #{attrs}
+        FROM #{table}
+        WHERE #{value[0]} AND  #{value[1]};
+       SQL
+
+       output
+
+     end
+
+     def where(attrs, value)
+       attrs = BlocRecord::Utility.convert_keys(attrs)
+       attrs.delete "id"
+       value = value.map { |key| BlocRecord::Utility.sql_strings(value[key]) }
+
+       output = connection.execute <<-SQL
+        SELECT #{attrs}
+        FROM #{table}
+        WHERE != #{value};
+       SQL
+
+       output
+
+     end
+   end
  end
-end
--- bloc_record/lib/bloc_record/persistence.rb
+++ bloc_record/lib/bloc_record/persistence.rb
@@ -25,11 +25,24 @@ module Persistence
 
     true
   end
+
   def save
     self.save! rescue false
   end
 
+  def update_attributes(attribute, value)
+    self.class.update(self.id, { attribute => value })
+  end
+
+  def update_attributes(updates)
+    self.class.update(self.id, updates)
+  end
+
   module ClassMethods
+    def update_all(updates)
+      update(nil, updates)
+    end
+
     def create(attrs)
       attrs = BlocRecord::Utility.convert_keys(attrs)
       attrs.delete "id"
@@ -45,6 +58,40 @@ module Persistence
       data = Hash[attributes.zip attrs.values]
       data["id"] = connection.execute("SELECT last_insert_rowid();")[0][0]
       new(data)
+    end
+
+    def update(ids, updates)
+      if updates.class == Hash
+        updates.each_with_index { |array, index| update(ids[index], array) }
+      else
+        updates = BlocRecord::Utility.convert_keys(updates)
+        updates.delete "id"
+
+        updates_array = updates.map { |key, value| "#{key}=#{BlocRecord::Utility.sql_strings(value)}" }
+      end
+
+      if ids.class == Fixnum
+        where_clause = "WHERE id = #{ids};"
+      elsif ids.class = Array
+        where_clause = ids.empty? ? ";" : "WHERE id in (#{ids.join(",")});"
+      else
+        where_clause = ";"
+      end
+
+      connection.execute <<-SQL
+        UPDATE #{table}
+        SET #{updates_array * ","} #{where_clause}
+        SQL
+
+        true
+    end
+
+    def self.method_missing(method_name, *args, &block)
+      if method_name.to_s =~ /update_(.*)/
+        update_($1, *args[0])
+      else
+        super
+      end
     end
   end
 end
