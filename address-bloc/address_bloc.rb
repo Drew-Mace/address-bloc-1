7fbda667373816affdcae5e1b42d6ea6f3bbd878
Drew-Mace <dremac1@live.com>
2017-11-02 12:17:04 -0400

    checkpoint 7 revisions

--- address-bloc/address_bloc.rb
+++ address-bloc/address_bloc.rb
@@ -1,8 +1,12 @@
 require_relative 'controllers/menu_controller'
 require 'bloc_record'
 
-BlocRecord.connect_to("db/address_bloc.db", :sqlite3)
-BlocRecord.connect_to("db/address_bloc.db", :pg)
+
+ if BlocRecord.database.type = "sqlite3"
+   BlocRecord.connect_to("db/address_bloc.db", :sqlite3)
+ elsif BlocRecord.database.type = "pg"
+   BlocRecord.connect_to("db/address_bloc.db", :pg)
+ end
 
 menu = MenuController.new
 system "clear"
